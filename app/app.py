e242097ca5b949aab7870247c55d058cd64bed04
sokolant <sokolant@cvut.cz>
2026-05-17 19:26:28 +0200

    feat: flask app basic structure

--- app/app.py
+++ app/app.py
@@ -1,1 +1,9 @@
-q
+from flask import Flask  # Importing the Flask module from the flask package  
+ 
+app = Flask(__name__)
+
+@app.route('/')  # View function for endpoint '/'  
+def hello():  
+    return "<h1>Hello, World!</h1>"
+if __name__ == "__main__":  
+    app.run(host='0.0.0.0', port=5000, debug=True)
